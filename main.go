2b559e698bb39915aaaf0bb1301b72d267e6106e
jesco00s <joseescobar197@gmail.com>
2025-11-17 12:27:56 -0600

    set up the skeleton for the addItem endpoint

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/items", itemsHandler)
-	fmt.Println("Hello World")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+type Item struct {
+	ResourceName string `json:"resourceName"`
+	TechDebtId   string `json:"techDebtId"`
 }
 
 func itemsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		addItem(w, r)
+	} else {
+		http.Error(w, "Unsupported method", http.StatusMethodNotAllowed)
+	}
+}
 
+func addItem(w http.ResponseWriter, r *http.Request) {
+	var newItem Item
+	err := json.NewDecoder(r.Body).Decode(&newItem)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+	fmt.Println(newItem)
+}
+
+func main() {
+	port := ":8080"
+	http.HandleFunc("/items", itemsHandler)
+	fmt.Println("Server is running on port", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
