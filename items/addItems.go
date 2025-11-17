bb6701092486cf62cb53ab7649eb09dc360aabc5
jesco00s <joseescobar197@gmail.com>
2025-11-17 16:52:56 -0600

    able to write to db

--- items/addItems.go
+++ items/addItems.go
@@ -19,6 +19,7 @@ func AddItem(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//todo need to separate this into its own function. Do not like this but it is ok for now
 	connStr := "user=postgres dbname=josejescobar host=localhost sslmode=disable"
 
 	// Connect to the database
@@ -28,5 +29,12 @@ func AddItem(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 	fmt.Println("err not equal to nil. can continue")
+
+	// Insert the item into the database
+	_, err = db.Exec("INSERT INTO public.tech_debt (resource_name, tech_debt_id) VALUES ($1, $2)", newItem.ResourceName, newItem.TechDebtId)
+	if err != nil {
+		http.Error(w, "Error saving item", http.StatusInternalServerError)
+		return
+	}
 
 }
